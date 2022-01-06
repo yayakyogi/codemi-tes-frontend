8ac6dc678dcfed44f6c39b604d5a83e8157fc106
yayakyogi <yayaktaka@gmail.com>
2022-01-06 21:52:01 +0700

    disable anchor-is-valid

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,10 +1,11 @@
+/* eslint-disable jsx-a11y/anchor-is-valid */
 import React, { useState } from "react";
 import Sidebar from "./Sidebar";
 
 function MenuRight() {
   return (
     <div className="navbar-menu-right">
-      <a href="/aa" style={{ marginRight: 20, position: "relative" }}>
+      <a href="#" style={{ marginRight: 20, position: "relative" }}>
         <img
           src="/icons/ic_notif.png"
           style={{ width: 30, height: 30 }}
@@ -27,7 +28,7 @@ function MenuRight() {
           <span style={{ color: "white", fontSize: 10 }}>1</span>
         </div>
       </a>
-      <a href="/#">
+      <a href="#">
         <img
           src="/img_dummy.jpg"
           style={{ width: 35, borderRadius: 100 }}
@@ -50,10 +51,12 @@ export default function Navbar() {
       {isShowSidebar ? <Sidebar isSidebarMobile={true} /> : <></>}
       <div className="navbar">
         <div className="container">
-          <img
-            src="https://files-id.codemi.co.id/files/learning/image/68c3ec1a6c5d155d748cde836d0289cb"
-            alt="logo-codemi"
-          />
+          <a href="/">
+            <img
+              src="https://files-id.codemi.co.id/files/learning/image/68c3ec1a6c5d155d748cde836d0289cb"
+              alt="logo-codemi"
+            />
+          </a>
           <div className="menu-toggle" onClick={() => showSideBar()}>
             <input type="checkbox" />
             <span></span>
