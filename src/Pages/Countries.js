0709aff6ebec942fff61ce8c8cd6008df41ca7d4
yayakyogi <yayaktaka@gmail.com>
2022-01-06 15:37:49 +0700

    add pages

---
+++ src/Pages/Countries.js
@@ -0,0 +1,52 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Navbar from "../Components/Navbar";
+import Sidebar from "../Components/Sidebar";
+import ActiveUser from "../Widget/ActiveUser";
+import Layout from "../Components/Layout";
+import { PieChart } from "../Components/Chart";
+
+export default function Countries() {
+  const [dataCovid, setDataCovid] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const loadDataCovid = async () => {
+    const res = await axios.get("https://covid19.mathdro.id/api/countries/IDN");
+    if (res.status === 200) {
+      setDataCovid(res.data);
+      setIsLoaded(true);
+    } else {
+      console.log("error");
+    }
+  };
+
+  useEffect(() => {
+    loadDataCovid();
+  }, [isLoaded]);
+
+  const labels = ["Confirmed", "Deaths", "Recovered"];
+  const data = [
+    dataCovid.confirmed?.value,
+    dataCovid.deaths?.value,
+    dataCovid.recovered?.value,
+  ];
+
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+      <Layout title="Cases Covid 19 on Indonesia">
+        <div style={{ width: "100%" }}>
+          {isLoaded !== true ? (
+            <h1>Loading...</h1>
+          ) : (
+            // <h1>Oke</h1>
+            <PieChart labels={labels} data={data} />
+          )}
+        </div>
+        <div style={{ width: 50, height: 50 }}></div>
+        <ActiveUser />
+      </Layout>
+    </>
+  );
+}
---
+++ src/Pages/Home.js
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Navbar from "../Components/Navbar";
+import Sidebar from "../Components/Sidebar";
+import ShowDataCovid from "../Widget/ShowDataCovid";
+import ActiveUser from "../Widget/ActiveUser";
+import Layout from "../Components/Layout";
+
+export default function Home() {
+  const [dataCovid, setDataCovid] = useState([]);
+  const [isLoaded, setIsLoaded] = useState(false);
+
+  const loadDataCovid = async () => {
+    const res = await axios.get("https://covid19.mathdro.id/api/");
+    if (res.status === 200) {
+      // console.log(res.data);
+      setDataCovid(res.data);
+      setIsLoaded(true);
+    } else {
+      console.log("error");
+    }
+  };
+
+  useEffect(() => {
+    loadDataCovid();
+  }, [isLoaded]);
+
+  return (
+    <>
+      <Navbar />
+      <Sidebar />
+      <Layout title="World Covid 19 Case">
+        <ShowDataCovid isLoaded={isLoaded} dataCovid={dataCovid} />
+        {/* <div style={{ width: 50 }}></div> */}
+        <ActiveUser />
+      </Layout>
+    </>
+  );
+}
