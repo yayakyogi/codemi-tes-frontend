fe3c443b7a1a75fa2ccaf6f9f777305ac2f555f8
yayakyogi <yayaktaka@gmail.com>
2022-01-06 21:46:56 +0700

    fix responsive and show sidebar

--- src/Components/Navbar.js
+++ src/Components/Navbar.js
@@ -1,54 +1,68 @@
-import React from "react";
+import React, { useState } from "react";
+import Sidebar from "./Sidebar";
+
+function MenuRight() {
+  return (
+    <div className="navbar-menu-right">
+      <a href="/aa" style={{ marginRight: 20, position: "relative" }}>
+        <img
+          src="/icons/ic_notif.png"
+          style={{ width: 30, height: 30 }}
+          alt="icon-notif"
+        />
+        <div
+          style={{
+            width: 15,
+            height: 15,
+            position: "absolute",
+            top: -20,
+            right: -5,
+            borderRadius: 2,
+            background: "red",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+          }}
+        >
+          <span style={{ color: "white", fontSize: 10 }}>1</span>
+        </div>
+      </a>
+      <a href="/#">
+        <img
+          src="/img_dummy.jpg"
+          style={{ width: 35, borderRadius: 100 }}
+          alt="user-profile"
+        />
+      </a>
+    </div>
+  );
+}
 
 export default function Navbar() {
+  const [isShowSidebar, setIsShowSidebar] = useState(false);
+
+  const showSideBar = () => {
+    setIsShowSidebar(!isShowSidebar);
+  };
+
   return (
-    <div className="navbar">
-      <div className="container">
-        <div style={{ display: "flex", alignItems: "center" }}>
-          <div className="menu-toggle">
+    <>
+      {isShowSidebar ? <Sidebar isSidebarMobile={true} /> : <></>}
+      <div className="navbar">
+        <div className="container">
+          <img
+            src="https://files-id.codemi.co.id/files/learning/image/68c3ec1a6c5d155d748cde836d0289cb"
+            alt="logo-codemi"
+          />
+          <div className="menu-toggle" onClick={() => showSideBar()}>
             <input type="checkbox" />
             <span></span>
             <span></span>
             <span></span>
           </div>
-          <img
-            src="https://files-id.codemi.co.id/files/learning/image/68c3ec1a6c5d155d748cde836d0289cb"
-            alt="logo-codemi"
-          />
-        </div>
-        <div>
-          <a href="/aa" style={{ marginRight: 20, position: "relative" }}>
-            <img
-              src="/icons/ic_notif.png"
-              style={{ width: 30, height: 30 }}
-              alt="icon-notif"
-            />
-            <div
-              style={{
-                width: 15,
-                height: 15,
-                position: "absolute",
-                top: -20,
-                right: -5,
-                borderRadius: 2,
-                background: "red",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-              }}
-            >
-              <span style={{ color: "white", fontSize: 10 }}>1</span>
-            </div>
-          </a>
-          <a href="/#">
-            <img
-              src="https://preview.keenthemes.com/metronic-v4/theme/assets/pages/media/profile/profile_user.jpg"
-              style={{ width: 35, borderRadius: 100 }}
-              alt="user-profile"
-            />
-          </a>
+          <MenuRight />
         </div>
       </div>
-    </div>
+    </>
   );
 }
