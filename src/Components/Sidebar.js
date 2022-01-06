d12c7e293e5875ffe5d341fca73ec6e93c08b13a
yayakyogi <yayaktaka@gmail.com>
2022-01-06 15:44:36 +0700

    add link

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,3 +1,4 @@
+import { Link } from "react-router-dom";
 import ItemSidebar from "./ItemSidebar";
 
 export default function Sidebar() {
@@ -6,17 +7,21 @@ export default function Sidebar() {
       <h1 className="title">HOME</h1>
       {/* Menu LEARNING */}
       <div className="menu">
-        <h2 className="sub-title">WORLD</h2>
-        <ItemSidebar
-          img="icons/ic_notif.png"
-          title="Courses"
-          showCounter={false}
-        />
-        <ItemSidebar
-          img="icons/ic_notif.png"
-          title="Learning Plants"
-          showCounter={false}
-        />
+        <h2 className="sub-title">DATA COVID 19</h2>
+        <Link to="/" style={{ textDecoration: "none" }}>
+          <ItemSidebar
+            img="icons/ic_notif.png"
+            title="World"
+            showCounter={false}
+          />
+        </Link>
+        <Link to="/countries" style={{ textDecoration: "none" }}>
+          <ItemSidebar
+            img="icons/ic_notif.png"
+            title="Indonesia"
+            showCounter={false}
+          />
+        </Link>
       </div>
       <div style={{ height: 200 }}></div>
     </div>
