6bea4a60af263797b09572ecc380c98268453fc1
yayakyogi <yayaktaka@gmail.com>
2022-01-06 21:53:01 +0700

    fix proptypes type

--- src/Components/Sidebar.js
+++ src/Components/Sidebar.js
@@ -1,11 +1,13 @@
 import { Link } from "react-router-dom";
+import propTypes from "prop-types";
 import ItemSidebar from "./ItemSidebar";
 
-export default function Sidebar() {
+export default function Sidebar(props) {
+  const { isSidebarMobile } = props;
+
   return (
-    <div className="sidebar">
+    <div className={isSidebarMobile ? "sidebar-mobile" : "sidebar"}>
       <h1 className="title">HOME</h1>
-      {/* Menu LEARNING */}
       <div className="menu">
         <h2 className="sub-title">DATA COVID 19</h2>
         <Link to="/" style={{ textDecoration: "none" }}>
@@ -23,7 +25,29 @@ export default function Sidebar() {
           />
         </Link>
       </div>
+      {isSidebarMobile && (
+        <>
+          <h1 className="title">User</h1>
+          <div className="menu">
+            <ItemSidebar
+              img="icons/ic_notif.png"
+              title="Notification"
+              showCounter={true}
+              count={1}
+            />
+            <ItemSidebar
+              img="icons/ic_user.png"
+              title="Akun"
+              showCounter={false}
+            />
+          </div>
+        </>
+      )}
       <div style={{ height: 200 }}></div>
     </div>
   );
 }
+
+Sidebar.propTypes = {
+  isSidebarMobile: propTypes.bool,
+};
